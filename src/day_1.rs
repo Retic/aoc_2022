fc2f47427a86c7c389dbe89da3f85b58ff5b54f1
Retic <deanezn@gmail.com>
2022-12-06 11:21:55 +0200

    Solved day 1 part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -14,6 +14,24 @@ pub fn solve_day_1_1(puzzle_input: &str) -> i32 {
     elves.iter().max().copied().unwrap_or(0)
 }
 
+pub fn solve_day_1_2(puzzle_input: &str) -> i32 {
+    let lines = puzzle_input.lines().map(|l| l);
+    let mut current_sum = 0;
+    let mut elves = Vec::new();
+
+    for line in lines {
+        if line != "" {
+            current_sum += line.parse::<i32>().unwrap();
+        } else {
+            elves.push(current_sum);
+            current_sum = 0;
+        }
+    }
+    elves.sort();
+
+    elves.iter().rev().take(3).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +40,11 @@ mod tests {
     fn test_elve_calorie_count() {
         let data = "1000\n2000\n3000\r\n\r\n4000\n5000\n6000\r\n\r\n7000\n8000\n9000\r\n\r\n10000";
         assert_eq!(solve_day_1_1(data), 24000)
+    }
+
+    #[test]
+    fn test_elve_calorie_count_of_top_3() {
+        let data = "1000\n2000\n3000\r\n\r\n4000\n5000\n6000\r\n\r\n7000\n8000\n9000\r\n\r\n10000";
+        assert_eq!(solve_day_1_2(data), 45000)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod day_1;
-use crate::day_1::solve_day_1_1;
+use crate::day_1::{solve_day_1_1, solve_day_1_2};
 
 fn main() {
     let puzzle_input_day_1 = include_str!("../puzzle_1.txt");
 
     //Day 1.1
     println!("{}", solve_day_1_1(puzzle_input_day_1));
+
+    //Day 1.2
+    println!("{}", solve_day_1_2(puzzle_input_day_1));
 }
