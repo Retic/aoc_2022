4e34d39f20d358f3e67452fa904b230de0f6ed09
Retic <deanezn@gmail.com>
2022-12-06 12:31:47 +0200

    solved day 2 part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,15 +1,74 @@
+const PAPER: i32 = 2;
+const ROCK: i32 = 1;
+const SCISSORS: i32 = 3;
+const WIN: i32 = 6;
+const DRAW: i32 = 3;
+const LOSS: i32 = 0;
+
+fn calculate_score(player_move: &str, opponent_move: &str) -> i32 {
+    let mut score = 0;
+    //let paper = 2;
+
+    match opponent_move {
+        "rock" => {
+            //Rock
+            match player_move {
+                "rock" => {
+                    score += ROCK;
+                    score += DRAW;
+                }
+                "paper" => {
+                    score += PAPER;
+                    score += WIN;
+                }
+                "scissors" => {
+                    score += SCISSORS;
+                    score += LOSS;
+                }
+                _ => {}
+            }
+        }
+        "paper" => match player_move {
+            "rock" => {
+                score += ROCK;
+                score += LOSS;
+            }
+            "paper" => {
+                score += PAPER;
+                score += DRAW;
+            }
+            "scissors" => {
+                score += SCISSORS;
+                score += WIN;
+            }
+            _ => {}
+        },
+        "scissors" => match player_move {
+            "rock" => {
+                score += ROCK;
+                score += WIN;
+            }
+            "paper" => {
+                score += PAPER;
+                score += LOSS;
+            }
+            "scissors" => {
+                score += SCISSORS;
+                score += DRAW;
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+
+    score
+}
+
 pub fn solve_day_2_1(puzzle_input: &str) -> i32 {
     let lines = puzzle_input.lines().map(|l| l);
     let mut score = 0;
 
     for line in lines {
-        let paper = 2;
-        let rock = 1;
-        let scissors = 3;
-        let win = 6;
-        let draw = 3;
-        let loss = 0;
-
         let result: Vec<&str> = line.split(" ").collect();
         let opponent_move = match result[0] {
             "A" => "rock",
@@ -23,57 +82,52 @@ pub fn solve_day_2_1(puzzle_input: &str) -> i32 {
             "Z" => "scissors",
             _ => "",
         };
-        match opponent_move {
-            "rock" => {
-                //Rock
-                match player_move {
-                    "rock" => {
-                        score += rock;
-                        score += draw;
-                    }
-                    "paper" => {
-                        score += paper;
-                        score += win;
-                    }
-                    "scissors" => {
-                        score += scissors;
-                        score += loss;
-                    }
-                    _ => {}
+
+        score += calculate_score(player_move, opponent_move);
+    }
+
+    score
+}
+
+pub fn solve_day_2_2(puzzle_input: &str) -> i32 {
+    let lines = puzzle_input.lines().map(|l| l);
+    let mut score = 0;
+
+    for line in lines {
+        let result: Vec<&str> = line.split(" ").collect();
+        let opponent_move = match result[0] {
+            "A" => "rock",
+            "B" => "paper",
+            "C" => "scissors",
+            _ => "",
+        };
+        let player_move = match result[1] {
+            "X" => {
+                //LOSS
+                match opponent_move {
+                    "rock" => "scissors",
+                    "paper" => "rock",
+                    "scissors" => "paper",
+                    _ => "",
                 }
             }
-            "paper" => match player_move {
-                "rock" => {
-                    score += rock;
-                    score += loss;
+            "Y" => {
+                //DRAW
+                opponent_move
+            }
+            "Z" => {
+                //WIN
+                match opponent_move {
+                    "rock" => "paper",
+                    "paper" => "scissors",
+                    "scissors" => "rock",
+                    _ => "",
                 }
-                "paper" => {
-                    score += paper;
-                    score += draw;
-                }
-                "scissors" => {
-                    score += scissors;
-                    score += win;
-                }
-                _ => {}
-            },
-            "scissors" => match player_move {
-                "rock" => {
-                    score += rock;
-                    score += win;
-                }
-                "paper" => {
-                    score += paper;
-                    score += loss;
-                }
-                "scissors" => {
-                    score += scissors;
-                    score += draw;
-                }
-                _ => {}
-            },
-            _ => {}
-        }
+            }
+            _ => "",
+        };
+
+        score += calculate_score(player_move, opponent_move);
     }
 
     score
@@ -87,5 +141,11 @@ mod test2s {
     fn test_rock_paper_scissors() {
         let data = "A Y\nB X\nC Z";
         assert_eq!(solve_day_2_1(data), 15)
+    }
+
+    #[test]
+    fn test_rock_paper_scissors_with_end_goal() {
+        let data = "A Y\nB X\nC Z";
+        assert_eq!(solve_day_2_2(data), 12)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod day_1;
 mod day_2;
 use crate::day_1::{solve_day_1_1, solve_day_1_2};
-use crate::day_2::solve_day_2_1;
+use crate::day_2::{solve_day_2_1, solve_day_2_2};
 
 fn main() {
     let puzzle_input_day_1 = include_str!("../puzzle_1.txt");
@@ -15,4 +15,7 @@ fn main() {
 
     //Day 2.1
     println!("{}", solve_day_2_1(puzzle_input_day_2));
+
+    //Day 2.2
+    println!("{}", solve_day_2_2(puzzle_input_day_2));
 }
